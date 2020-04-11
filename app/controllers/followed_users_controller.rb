39ae591b971d2d28dcfcdbfb00200e06ad33e985
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2020-04-11 12:04:06 +0200

    Add counter to followers and followed_users

--- app/controllers/followed_users_controller.rb
+++ app/controllers/followed_users_controller.rb
@@ -3,13 +3,13 @@ class FollowedUsersController< ApplicationController
   def create
     user_to_follow = find_user
     current_user.follow(user_to_follow)
-    redirect_to user_to_follow
+    redirect_to user_path(user_to_follow.username)
   end
 
   def destroy
     user_to_not_follow = find_user
     current_user.to_not_follow(user_to_not_follow)
-    redirect_to user_to_not_follow
+    redirect_to user_path(user_to_not_follow.username)
   end
 
   private
