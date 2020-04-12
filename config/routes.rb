4f1996df171058e36296524310d867699aadc6b6
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2020-04-12 10:17:45 +0200

    Refactore create action into ShoutsController

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   end
 
   root to: "home#show", as: nil
-  resources :shouts, only: [:create, :show] do
+  post "text_shouts" => "shouts#create", default: { content_type: TextShout }
+  post "photo_shouts" => "shouts#create", default: { content_type: PhotoShout }
+
+  resources :shouts, only: [:show] do
     member do
       post "like" => "likes#create"
       delete "unlike" => "likes#destroy"
