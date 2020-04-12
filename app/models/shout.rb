d6cc79ca92ca3c47ce7d5d43af8d8fbd522bfa83
David Ruiz de la Rosa <david.ruizdelarosa@gmail.com>
2020-04-12 11:57:45 +0200

    Search Basic with scope into Shout

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -5,4 +5,10 @@ class Shout < ApplicationRecord
   delegate :username, to: :user
 
   belongs_to :content, polymorphic: true
+
+  scope :search, -> (term:) {
+    joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id").
+        where("text_shouts.body LIKE ?", "%##{term}%")
+  }
+
 end
